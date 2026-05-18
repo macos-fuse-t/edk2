a55ca5f7107ac53aad6a27122d7fe3750414a3cd
Alex <alex@fuse-t.org>
2026-05-18 12:26:11 +0300

    OvmfPkg/Scorpi: reserve hwinfo memory ranges

--- OvmfPkg/Scorpi/ScorpiPlatformPei/ScorpiPlatformPei.c
+++ OvmfPkg/Scorpi/ScorpiPlatformPei/ScorpiPlatformPei.c
@@ -176,6 +176,7 @@ ScorpiPublishRange (
       break;
     case SCORPI_X64_ENTRY_RESERVED_RANGE:
       PlatformAddReservedMemoryBaseSizeHob (Range->Base, Range->Size, FALSE);
+      BuildMemoryAllocationHob (Range->Base, Range->Size, EfiReservedMemoryType);
       break;
     default:
       break;
